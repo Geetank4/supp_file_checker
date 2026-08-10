83f00735bd01b4c1ae7f473bfb7fc0fb95623683
Geetank4 <136714579+Geetank4@users.noreply.github.com>
2026-08-10 11:21:54 +0530

    Fix: disable SSR for pdfjs-dist

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,8 @@
-export { default } from "../supp-checker";
+import dynamic from "next/dynamic";
+
+const SuppChecker = dynamic(() => import("../supp-checker"), { ssr: false });
+
+export default function Home() {
+  return <SuppChecker />;
+}
+
